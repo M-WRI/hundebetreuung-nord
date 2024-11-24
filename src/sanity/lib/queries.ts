d549063a386775aac1cfd15f963e75126ca64075
Moritz Wright <moritzwright@Moritzs-MacBook-Pro.local>
2024-11-24 21:45:00 +0100

    cleanup

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,7 +1,5 @@
-// Query to fetch all page slugs
 export const pagePathsQuery = `*[_type == "page"]{ "slug": slug.current }`;
 
-// Query to fetch page data by slug
 export const pageQuery = `*[_type == "page" && slug.current == $slug][0]{
   title,
   slug
